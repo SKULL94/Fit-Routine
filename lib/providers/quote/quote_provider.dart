90e7360bdab9391f4bdcaba8a2ff4904d77616d0
SKULL94 <zeeshanhaider794@gmail.com>
2025-06-13 17:42:58 +0530

    feat(quote): quote provider integrated

--- lib/providers/quote/quote_provider.dart
+++ lib/providers/quote/quote_provider.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
+import 'package:fit_routine_app/core/extensions/better_client.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:http/http.dart' as http;
 import '../../models/quote/quote.dart';
 part 'quote_provider.g.dart';
 
 @Riverpod(keepAlive: true)
 Future<Quote> getQuote(Ref ref) async {
   final url = Uri.parse('https://quotes-api-self.vercel.app/quote');
-  final client = http.Client();
+  final client = await ref.getBetterClient();
   final response = await client.get(url);
   if (response.statusCode == 200) {
     final body = jsonDecode(response.body) as Map<String, dynamic>;
